98ca2e8bef4aa1033f6c818409bda2d992cbd2ea
twkevinzhang <twkevinzhang@gmail.com>
2025-02-04 01:09:24 +0800

    feat: implement cli by lib github.com/urfave/cli/v2

---
+++ cmd/checksum.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+func checksumCmd(c *cli.Context) error {
+	return nil
+}
---
+++ cmd/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"os"
+)
+
+// version of the futil tool
+const version = "0.0.1"
+
+func main() {
+	app := &cli.App{
+		Name:    "futil",
+		Usage:   "File Utility",
+		Version: version,
+		Commands: []*cli.Command{
+			{
+				Name:      "linecount",
+				Usage:     "Print line count of file",
+				UsageText: "futil linecount [flags]",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "file",
+						Aliases: []string{"f"},
+						Usage:   "the input file",
+					},
+				},
+				Action: lineCountCmd,
+			},
+			{
+				Name:      "checksum",
+				Usage:     "Print checksum of file",
+				UsageText: "futil checksum [flags]",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "file",
+						Aliases: []string{"f"},
+						Usage:   "the input file",
+					},
+					&cli.BoolFlag{
+						Name:  "md5",
+						Usage: "calculate MD5 checksum",
+					},
+					&cli.BoolFlag{
+						Name:  "sha1",
+						Usage: "calculate SHA1 checksum",
+					},
+					&cli.BoolFlag{
+						Name:  "sha256",
+						Usage: "calculate SHA256 checksum",
+					},
+				},
+				Action: checksumCmd,
+			},
+			{
+				Name:      "version",
+				Usage:     "Show the version info",
+				UsageText: "futil version",
+				Action: func(c *cli.Context) error {
+					fmt.Printf("futil v%s\n", version)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
